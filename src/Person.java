822b9e12a12196e4ad7dd1f8dddc14977ffce85c
uthej002 <uthej002@gmail.com>
2022-01-22 15:19:02 +0530

    added a new class person.java

---
+++ src/Person.java
@@ -0,0 +1,38 @@
+public class Person {
+    private String name = null;
+    private int height = 0;
+    private int weight = 0;
+    private int age = 0;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
